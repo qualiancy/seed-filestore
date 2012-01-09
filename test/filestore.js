1cf8b01f6ecda42483bd5653c8f72d071c25e1fc
Jake Luer <jake.luer@gmail.com>
2012-01-09 00:53:48 -0500

    all filestore model tests

--- test/filestore.js
+++ test/filestore.js
@@ -18,18 +18,40 @@ describe('Seed FileStore', function () {
   describe('Models', function () {
 
     var Person = Seed.Model.extend('traveller', {
-      store: Store
+        store: Store
     });
 
     var arthur = new Person({
-      name: 'Arthur Dent',
-      occupation: 'traveller'
+        name: 'Arthur Dent'
+      , occupation: 'traveller'
     });
 
     it('should allow saving', function (done) {
       arthur.save(function (err) {
         var id = this.id;
         should.not.exist(err);
+        path.existsSync(path.join(dir, this.type, id + '.json')).should.be.true;
+        done();
+      });
+    });
+
+    it('should allow retrieval', function (done) {
+      var arthur2 = new Person({
+          id: arthur.id
+      });
+
+      arthur2.fetch(function (err) {
+        should.not.exist(err);
+        arthur2.get('name').should.equal(arthur.get('name'));
+        done();
+      });
+    });
+
+    it('should allow for removal', function (done) {
+      arthur.destroy(function (err) {
+        var id = this.id;
+        should.not.exist(err);
+        path.existsSync(path.join(dir, this.type, id + '.json')).should.be.false;
         done();
       });
     });
